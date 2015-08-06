1afd78666a9e22cf1866cb5adf0114a38faddaba
goodjob1114 <goodjob1114@gmail.com>
2015-08-06 20:03:43 +0800

    ex-02-primitives

---
+++ ex-02-primitives/primitives.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;
+    let an_integer   = 5i32;
+
+    let default_float   = 3.0;
+    let default_integer = 7;
+
+    let mut mutable = 12;
+
+    // The type of a variable must be the same type. 
+    mutable = 168;
+}
